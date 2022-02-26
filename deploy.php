3efea236bd206be16a335e3f730166a0c6690808
Jamie Gardner <jamie@thatgardnerone.com>
2022-02-26 13:28:26 +0000

    Use Deployer

---
+++ deploy.php
@@ -0,0 +1,33 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+
+set('application', 'thatgardnerone.com');
+
+// Config
+
+set('repository', 'git@github.com:thatgardnerone/thatgardnerone');
+set('branch', 'main');
+
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('web01.production.tgo')
+    ->set('deploy_path', '/srv/{{application}}');
+
+// Tasks
+
+task('npm:build', function () {
+    cd('{{release_path}}');
+    run('npm ci && npm run prod');
+});
+
+before('deploy:symlink', 'npm:build');
+
+after('deploy:failed', 'deploy:unlock');
